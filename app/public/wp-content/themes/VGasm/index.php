7b77209d815fcd31e24db41251008d94eb48bb73
jasonaul <jason.aul@gmail.com>
2023-03-02 23:28:09 -0500

    learning some PHP basics again

---
+++ app/public/wp-content/themes/VGasm/index.php
@@ -0,0 +1,10 @@
+<?php 
+ function greeting($name, $color) {
+    echo "<p>Hi, my name is $name and my favorite color is $color.</p>";
+ }
+
+greeting('Jason', 'blue');
+greeting('Simone', 'red');
+?>
+
+<h1><?php bloginfo('name'); ?></h1>
