2a3c6fdeef456166a30aee38ea845c559634e5de
jasonaul <jason.aul@gmail.com>
2023-03-02 23:39:53 -0500

    loopy

--- app/public/wp-content/themes/VGasm/index.php
+++ app/public/wp-content/themes/VGasm/index.php
@@ -1,4 +1,47 @@
 <?php 
+
+$names = array('Jason', 'Simone', 'Stacie', 'Kevin');
+
+$count = 0;
+
+while($count < count($names)) {
+    echo "<li>Hi, my name is $names[$count]</li>";
+    $count++;
+}
+
+?>
+
+
+
+
+
+
+
+
+<!-- <?php 
+
+$count = 1;
+
+while($count <= 100) {
+    echo "<li>$count</li>";
+    $count++;
+}
+
+?> -->
+
+
+
+
+
+<!-- <?php 
+
+$myName = "Jason";
+
+?>
+
+<p>Hi, my name is <?php echo $myName ?></p> -->
+
+<!-- <?php 
  function greeting($name, $color) {
     echo "<p>Hi, my name is $name and my favorite color is $color.</p>";
  }
@@ -8,3 +51,4 @@ greeting('Simone', 'red');
 ?>
 
 <h1><?php bloginfo('name'); ?></h1>
+<p><?php bloginfo('description'); ?></p> -->
