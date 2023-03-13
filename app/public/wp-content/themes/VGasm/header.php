e350d34e7c653256d420bb80d94c36139cb6fdb4
jasonaul <jason.aul@gmail.com>
2023-03-13 00:30:54 -0400

    working on a new header:

--- app/public/wp-content/themes/VGasm/header.php
+++ app/public/wp-content/themes/VGasm/header.php
@@ -8,4 +8,46 @@
     <title>VGasm</title>
 </head>
 <body>
-    <h1>Wowzers! A Header!</h1>
+
+
+<header class="site-header-main">
+    <!-- Header section -->
+
+<div class="top-header">
+  <a href="#" title="VGasm" class="top-header-logo-img">
+    <div class="top-header-div2">
+      <div class="top-header-div3">
+        <img src=<?php echo get_theme_file_uri('/images/logo.png') ?> alt="logo" width="280" height="40">
+      </div>
+    </div>
+  </a>
+  <div class="header-alt"> | A Reader Is You.</div>
+</div>
+
+
+<!-- 
+<header class="site-header">
+      <div class="container">
+        <h1 class="school-logo-text float-left">
+          <a href="#"><strong>Fictional</strong> University</a>
+        </h1>
+        <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+        <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
+        <div class="site-header__menu group">
+          <nav class="main-navigation">
+            <ul>
+              <li><a href="#">About Us</a></li>
+              <li><a href="#">Programs</a></li>
+              <li><a href="#">Events</a></li>
+              <li><a href="#">Campuses</a></li>
+              <li><a href="#">Blog</a></li>
+            </ul>
+          </nav>
+          <div class="site-header__util">
+            <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
+            <a href="#" class="btn btn--small btn--dark-orange float-left">Sign Up</a>
+            <span class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+          </div>
+        </div>
+      </div>
+    </header> -->
